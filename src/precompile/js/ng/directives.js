29667975c0d3f94def147609e7ea759864b7aeee
Nate Clark <nclark@knotice.com>
2015-01-14 07:27:00 -0500

    Added angular resources

---
+++ src/precompile/js/ng/directives.js
@@ -0,0 +1,38 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('weather.directives', [
+      'app.services'
+    ])
+    .directive('weatherIcon', weatherIcon)
+    ;
+
+  /* @ngInject */
+  function weatherIcon(IconService) {
+    return {
+      restrict: 'E',
+      scope: {
+        icon: '='
+      },
+      replace: true,
+      template: '<canvas></canvas>',
+      link: linkFn
+    };
+
+    function linkFn(scope, element, attrs){
+      scope.$watch('icon', function onIconChange(newVal){
+        if(!newVal) return;
+
+        var ico = newVal.toUpperCase().split('-').join('_');
+
+        if(!Skycons[ico]) return;
+
+        IconService.remove(element[0]);
+        IconService.add(element[0], Skycons[ico]);
+        IconService.play();
+      });
+    }
+  }
+  weatherIcon.$inject = ['IconService'];
+})();
